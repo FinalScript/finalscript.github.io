67f3b5688b90b758c323afe03af9a876b45f3e78
Roynul Rohan <roynulrohan@gmail.com>
2022-04-22 06:33:54 -0400

    Test deployment

---
+++ config/index.ts
@@ -0,0 +1,12 @@
+import Web3 from 'web3';
+
+export const networkConfig = {
+    chainName: 'Avalanche Testnet',
+    chainId: Web3.utils.toHex(43113),
+    nativeCurrency: { name: 'AVAX', decimals: 18, symbol: 'AVAX' },
+    rpcUrls: ['https://api.avax-test.network/ext/bc/C/rpc'],
+};
+
+export const minerConfig = {
+    contractAddress: '0x2ffe9547d4272478De56CE60CE013f8cA8dDA6BE',
+};
