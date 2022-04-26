05a8926fadf842d806b14dc3af44fe08bdd16397
Roynul Rohan <roynulrohan@gmail.com>
2022-04-25 21:26:31 -0400

    Public assets structure changed and font issue fixed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -15,10 +15,6 @@ const Document = () => {
                 <link rel='icon' type='image/png' sizes='32x32' href='/favicon-32x32.png' />
                 <link rel='icon' type='image/png' sizes='16x16' href='/favicon-16x16.png' />
                 <link rel='manifest' href='/site.webmanifest' />
-                <link rel='preconnect' href='https://fonts.googleapis.com' />
-                <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin={'true'} />
-                <link href='/assets/fonts/VPPixel-Simplified.otf' rel='stylesheet' />
-                <link href='//db.onlinewebfonts.com/c/98e6dd769068c2091e7413443f0b0c5e?family=VPPixel-Simplified' rel='stylesheet' type='text/css' />
             </Head>
             <body>
                 <Main />
