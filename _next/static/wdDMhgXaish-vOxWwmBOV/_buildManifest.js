403c253ad9e25069c0f8fdcc76680b112dbc8eb2
roynulrohan <roynulrohan@users.noreply.github.com>
2022-04-26 12:55:36 +0000

    deploy: 83e900c268ffca7828cd2556a4569567cca6802c

--- _next/static/wdDMhgXaish-vOxWwmBOV/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/5-fedd7f1481f6f684.js","static/chunks/pages/index-855538dddd199b05.js"],"/_error":["static/chunks/pages/_error-5ea477d15194794a.js"],"/tos":["static/chunks/pages/tos-3b6ba96202a0e82b.js"],sortedPages:["/","/_app","/_error","/tos"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
