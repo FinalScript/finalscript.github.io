83e900c268ffca7828cd2556a4569567cca6802c
Roynul Rohan <roynulrohan@gmail.com>
2022-04-26 08:52:48 -0400

    Added boilerplate for whitepaper

---
+++ pages/whitepaper.tsx
@@ -0,0 +1,20 @@
+import { NextPage } from 'next';
+import Head from 'next/head';
+import React from 'react';
+
+const whitepaper: NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Whitepaper | MinerVerse</title>
+            </Head>
+            <section className='text-gray-400 bg-gray-900 body-font h-screen flex justify-center items-center'>
+                <div>
+                    Whitepaper
+                </div>
+            </section>
+        </>
+    );
+};
+
+export default whitepaper;
